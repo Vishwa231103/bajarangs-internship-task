fe95290fa54befd911ae3f5e8b6c83f1688fb319
Vishwateja Pothula <vishwapothula23@gmail.com>
2025-12-06 17:40:26 +0530

    Remove build and server configuration from Vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: path.resolve(__dirname, '../backend/public'),
-    emptyOutDir: true,
-  },
-  server: {
-    port: 5173,
-  },
-});
+})
